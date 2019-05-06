4843d7c3a0bafb6092a1ce439e3e057fb09d5c29
jiayu5 <44888668+jiayu5@users.noreply.github.com>
2019-05-06 14:29:04 -0500

    Add files via upload

--- src/components/class/ClassCommentCard.js
+++ src/components/class/ClassCommentCard.js
@@ -9,20 +9,20 @@ class ClassCommentCard extends React.Component {
                 <div className="meta">{this.props.time}</div>
                 <div className="description">
                 {/* data percentage should be 100 - width to relect correct color */}
-                <div className="ui indicating progress" data-percent={`${11-this.props.difficulty}0`}>
-                    <div className="bar" style={{transitionDuration: "300ms", width: `${this.props.difficulty}0%`}}></div>
-                    <div className="label">Difficulty Level</div>
+                <div class="ui indicating progress" data-percent={`${11-this.props.difficulty}0`}>
+                    <div class="bar" style={{transitionDuration: "300ms", width: `${this.props.difficulty}0%`}}></div>
+                    <div class="label">Difficulty Level</div>
                 </div>
-                <div className="ui indicating progress" data-percent={`${11-this.props.workload}0`}>
-                    <div className="bar" style={{transitionDuration: "300ms", width: `${this.props.workload}0%`}}></div>
-                    <div className="label">Workload</div>
+                <div class="ui indicating progress" data-percent={`${11-this.props.workload}0`}>
+                    <div class="bar" style={{transitionDuration: "300ms", width: `${this.props.workload}0%`}}></div>
+                    <div class="label">Workload</div>
                 </div>
                 <p>{this.props.comment}</p>
                 </div>
             </div>
             <div className="extra content">
                 <i className="check icon"></i>
-                2 Votes
+                121 Votes
             </div>
             </div>
         )
